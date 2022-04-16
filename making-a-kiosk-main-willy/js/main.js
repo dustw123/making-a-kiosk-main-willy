c2b71fa48b0a8817924d4dba1fff93436fe8c5ce
lawshop <ddd40510@gmail.com>
2022-04-16 15:27:27 +0900

    0416

--- making-a-kiosk-main-willy/js/main.js
+++ making-a-kiosk-main-willy/js/main.js
@@ -5,7 +5,7 @@ window.onload = () => {
 let menu = [];
 
 menu = {
-    menuCoffee : [
+    coffee : [
         { name : "아메리카노", price : 3000, num : 1 },
         { name : "아이스 아메리카노", price : 3500, num : 1 },
         { name : "카페라떼", price : 4000, num : 1 },
@@ -17,7 +17,7 @@ menu = {
         { name : "카페모카", price : 4500, num : 1 },
         { name : "아이스 카페모카", price : 4500, num : 1 }
     ],
-    menuTea: [
+    tea: [
         { name : "얼그레이", price : 3000, num : 1 },
         { name : "아이스 얼그레이", price : 3500, num : 1 },
         { name : "잉글리쉬브랙퍼스트", price : 3000, num : 1 },
@@ -33,7 +33,7 @@ menu = {
         { name : "핫초콜릿", price : 3000, num : 1},
         { name : "아이스 초콜릿", price : 3500, num : 1}
     ],
-    menuSandwich: [
+    sandwich: [
         { name : "잠봉뵈르", price : 6000, num : 1 },
         { name : "치킨클럽샌드위치", price : 6500, num : 1 },
         { name : "BLT샌드위치", price : 6500, num : 1}
@@ -47,6 +47,7 @@ for(key in menu){
 
     let ul = document.createElement('ul');
     ul.className = 'menu_area';
+    ul.id = `menu_${key}`;
 
 
     for( let i = 0; i < menu[key].length; i++){
@@ -72,9 +73,7 @@ for(key in menu){
         // let menuTeaTab = document.getElementById('tab_menu_tea');
         // let menuSandwichTab = document.getElementById('tab_menu_sandwich');
 
-        let menuCoffee = document.getElementById('menu_coffee');
-        let menuTea = document.getElementById('menu_tea');
-        let menuSandwich = document.getElementById('menu_sandwich');
+
 
         // 스코프 확인!!
 
@@ -82,34 +81,36 @@ for(key in menu){
             case를 만들어서 보여주는 것
             현재 case 3개가 필요하다. 
         */
-        function showMenu(subMenu){
 
-            switch(subMenu){
-                case 1: 
-                menuCoffee.style.display = "block";
-                break;
-
-                case 2:
-                menuTea.style.display = "block";  
-                break;
-
-                case 3:
-                menuSandwich.style.display = "block";  
-                break;
-            }
-        }
-        showMenu();
-        console.log(showMenu);
-
-    }
-
-    // console.log(ul);
-
-
-
+        console.log(ul);
+    } //for문 끝
 
 
 } //for in 끝
 
 
+
 } //window.onload 끝
+
+function showMenu(subMenu){
+    let menuCoffee = document.getElementsByClassName('menu_coffee');
+    let menuTea = document.getElementsByClassName('menu_tea');
+    let menuSandwich = document.getElementsByClassName('menu_sandwich');
+
+
+    switch(subMenu){
+        case 1: 
+        menuCoffee[0].style.display = "block";//ㅇㅔㄹ리먼트가 배열이라 이ㅎ게 뽑ㅏ준다.
+        break;
+
+        case 2:
+        menuTea[0].style.display = "block";  
+        break;
+
+        case 3:
+        menuSandwich[0].style.display = "block";  
+        break;
+    }
+
+
+}
